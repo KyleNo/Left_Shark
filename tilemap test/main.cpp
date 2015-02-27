4f11b1e6790dbe425ea0cc816b985dba8958c8a5
Bill-Cosby <djhartman1216@gmail.com>
2015-02-27 10:42:13 -0800

    New base with TMX parser
    No code yet

--- tilemap test/main.cpp
+++ tilemap test/main.cpp
@@ -1,6 +1,4 @@
-#include "resources/rapidjson/rapidjson.h"
-#include "resources/rapidjson/filestream.h"
-#include "resources/rapidjson/document.h"
+#include "resources/TMXParser.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -17,28 +15,5 @@ public:
 
 int main()
 {
-    loadedmap shittyshit;
-    FILE * pFile = fopen ("minimal.json" , "r");
-    rapidjson::FileStream is(pFile);
-    rapidjson::Document document;
-    document.ParseStream<0>(is);
-    const rapidjson::Value& a = document["data"];
-//    assert(document["height"].IsInt());
-//    assert(document["height"].IsInt());
-//    shittyshit.height=document["height"];
-//    shittyshit.width=document["width"];
-shittyshit.height=100;
-shittyshit.width=100;
-    for (rapidjson::SizeType i=0;i<sizeof(a);i++)
-    {
-        shittyshit.data.push_back(a[i].GetInt());
-    }
-    for (int i=0;i<shittyshit.height;i++)
-    {
-        for (int j=0;j<shittyshit.width;j++)
-        {
-            cout << shittyshit.data[i*shittyshit.height+j];
-        }
-        cout << endl;
-    }
+
 }
