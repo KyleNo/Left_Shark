3d1ce4c5e2927263a1e67eb5ce35aa65223584c1
KyleNo <kylenoble8@gmail.com>
2015-04-03 10:53:12 -0700

    moving with mouse

--- yeeeee/main.h
+++ yeeeee/main.h
@@ -232,10 +232,10 @@ void tileDraw(sf::RenderWindow& window, int numberofcharacterspossible)
             screenText.setString("Tile Validity Shown");
             checkingValidity = true;
         }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::M)){//screen movement
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::M)or true){//screen movement
             screenText.setString("View movement enabled, use the arrow keys");
-            cout << testmap.width;
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) and view1.getCenter().x < testmap.width*32-400){//right
+            cout << sf::Mouse::getPosition(window).x << "\t" << sf::Mouse::getPosition(window).y << endl;
+            if(sf::Mouse::getPosition(window).x > 768 and view1.getCenter().x < testmap.width*32-400){//right
                 buttonPressed = true;
                 framerateCounter++;
                 if(((buttonPressed == true) && (framerateCounter == 1)) || (framerateCounter >= 30)){
@@ -243,7 +243,7 @@ void tileDraw(sf::RenderWindow& window, int numberofcharacterspossible)
                     viewCounterX+=32;
                     window.setView(view1);
                 }}
-            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) and view1.getCenter().x>400 ){//left
+            else if(sf::Mouse::getPosition(window).x < 32 and view1.getCenter().x>400 ){//left
                 buttonPressed = true;
                 framerateCounter++;
                 if(((buttonPressed == true) && (framerateCounter == 1)) || (framerateCounter >= 30)){
@@ -252,7 +252,7 @@ void tileDraw(sf::RenderWindow& window, int numberofcharacterspossible)
                     window.setView(view1);
 
                 }}
-            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) and view1.getCenter().y > 320){//up
+            else if(sf::Mouse::getPosition(window).y < 32 and view1.getCenter().y > 320){//up
                 buttonPressed = true;
                 framerateCounter++;
                 if(((buttonPressed == true) && (framerateCounter == 1)) || (framerateCounter >= 30)){
@@ -260,7 +260,7 @@ void tileDraw(sf::RenderWindow& window, int numberofcharacterspossible)
                         viewCounterY-=32;
                         window.setView(view1);
                 }}
-            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) and view1.getCenter().y < testmap.height*32-320){//down
+            else if(sf::Mouse::getPosition(window).y > 608 and view1.getCenter().y < testmap.height*32-320){//down
                 buttonPressed = true;
                 framerateCounter++;
                 if(((buttonPressed == true) && (framerateCounter == 1)) || (framerateCounter >= 30)){
